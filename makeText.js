494fc5fcda13576a57566e13b721d6caf671235d
pixls <tha.pixls@gmail.com>
2022-06-02 13:01:01 -0400

    making chains works

--- makeText.js
+++ makeText.js
@@ -1,2 +1,9 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const { MarkovMachine } = require("./markov");
+const argv = process.argv;
+let args = argv[2];
 
+let mm = new MarkovMachine(args);
+
+console.log(mm);
--- markov.js
+++ markov.js
@@ -17,9 +17,26 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let uniqueWords = [...new Set(this.words)];
+    this.chains = {};
+    console.log(uniqueWords)
+    for (let uniqueWord of uniqueWords) {
+      for (let word of this.words) {
+        if (word == uniqueWord) {
+          let i = this.words.indexOf(word);
+          let nextWord = this.words[i + 1];
+          if (this.chains[word]) {
+            this.chains[word].push(nextWord);
+            console.log("True")
+          }
+          else {
+            this.chains[word] = [nextWord];
+            console.log("False")
+          }
+        }
+      }
+    }
   }
-
 
   /** return random text from chains */
 
@@ -27,3 +44,5 @@ class MarkovMachine {
     // TODO
   }
 }
+
+module.exports = { MarkovMachine }
